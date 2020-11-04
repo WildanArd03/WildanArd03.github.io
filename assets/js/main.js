e44458d74abfacb080b068773779cfaf03afe4db
WildanArd03 <karinagusion@gmail.com>
2020-11-05 02:38:45 +0700

    :) update gan

--- assets/js/main.js
+++ assets/js/main.js
@@ -164,34 +164,6 @@
       }
     }
   });
-
-  //pagination
-  (document).ready(function() {
-
-    $(".owl-carousel").owlCarousel({
-    
-    autoPlay: 3000,
-    items : 4,
-    itemsDesktop : [1199,3],
-    itemsDesktopSmall : [979,3],
-    center: true,
-    nav:true,
-    loop:true,
-    responsive: {
-    600: {
-    items: 4
-    }
-    }
-    
-    
-    
-    
-    
-    
-    });
-    
-    });
-
   // Porfolio isotope and filter
   $(window).on('load', function() {
     var portfolioIsotope = $('.portfolio-container').isotope({
